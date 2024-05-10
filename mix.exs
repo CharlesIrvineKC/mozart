481ee953b8d1b520858925578e5ca1147550e681
Chuck Irvine <chuck.irvine@icloud.com>
2024-05-10 11:51:40 -0500

    make :observe available

--- mix.exs
+++ mix.exs
@@ -14,7 +14,7 @@ defmodule Mozart.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger],
+      extra_applications: [:logger, :runtime_tools, :observer, :wx],
       mod: {Mozart.Application, []}
     ]
   end
