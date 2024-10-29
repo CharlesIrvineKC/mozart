2d0f093d4c339907ddf677e2dc663517dfc6928b
Chuck Irvine <chuck.irvine@icloud.com>
2024-10-29 11:23:06 -0500

    bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Mozart.MixProject do
   def project do
     [
       app: :mozart,
-      version: "1.0.0",
+      version: "1.0.1",
       elixir: "~> 1.16",
       elixirc_paths: ["lib", "test/support"],
       start_permanent: Mix.env() == :prod,
